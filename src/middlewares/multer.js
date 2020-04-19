2cba0e760291c7e446b1f1d7c681d897143e9d51
unknown <fegoworks@hotmail.com>
2020-04-19 22:14:04 +0100

    feat(file-upload): build middleware - setup cloudinary - setup multer

---
+++ src/middlewares/multer.js
@@ -0,0 +1,39 @@
+/* eslint-disable indent */
+/* eslint-disable operator-linebreak */
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, './src/uploads/');
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${new Date().toISOString().replace(/:/g, '-')}${file.originalname}`
+    );
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'file/text'
+  ) {
+    cb(null, true);
+  } else {
+    // reject file
+    cb({
+        message: 'Unsupported file format',
+      },
+      false);
+  }
+};
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 1024 * 1024,
+  },
+  fileFilter,
+});
+
+export default upload;
